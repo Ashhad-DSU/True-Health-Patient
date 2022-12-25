f38458414d02f0acbedf3d2ced3e308f76a9d571
Muhammad Ashhad Safwan <m_ashhadhlr@yahoo.com>
2022-12-25 18:43:05 +0500

    Added the first SVG file on Navbar

---
+++ lib/home_screen_navbar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+
+class HomeScreenNavbar extends StatelessWidget {
+  const HomeScreenNavbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          height: 24,
+          width: 24,
+          decoration: BoxDecoration(
+            image: DecorationImage(image: Svg('assets/svg/bars-solid.svg', size: Size()))
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../home_screen_navbar.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child:SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const HomeScreenNavbar(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
