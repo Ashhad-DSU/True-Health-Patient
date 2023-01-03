cd54034b7dbc50d31d2af39675d0677136e61607
Muhammad Ashhad Safwan <m_ashhadhlr@yahoo.com>
2023-01-03 18:39:17 +0500

    App Grid added on dashboard

--- lib/components/doctor_app_grid_menu.dart
+++ lib/components/doctor_app_grid_menu.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 
 import '../models/doctor.dart';
 
@@ -14,6 +15,44 @@ class DoctorAppGridMenu extends StatelessWidget {
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4, mainAxisSpacing: 8),
       padding: EdgeInsets.zero,
       itemCount: doctorMenu.length,
+      itemBuilder: (BuildContext context, int index) {return GestureDetector(
+        child: Container(
+          constraints: BoxConstraints(
+            maxHeight: 8,
+
+          ),
+          child: Column(
+            children: [
+              Container(
+                constraints: BoxConstraints(
+                  minHeight: 56,
+                  minWidth: 56,
+                  maxHeight: 69,
+                  maxWidth: 69,
+
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: Svg('assets/svg${doctorMenu[index].image}'),
+                  )
+                ),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Expanded(
+                child: Text(
+                doctorMenu[index].name,
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              ),
+            ],
+          ),
+        ),
+      ); },
+
     );
   }
 }
